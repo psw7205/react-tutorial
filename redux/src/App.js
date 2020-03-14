54cbcc126987a2e61caf92cf0b0f12809dd7bbd0
psw7205-mac <psw7205@gmail.com>
2020-03-14 12:19:47 +0900

    Todo

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./test";
 import CounterContainer from "./components/Counter";
+import TodosContainer from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
       <CounterContainer />
+      <TodosContainer />
     </div>
   );
 }
--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { increase, decrease, setDiff } from "../modules/counter";
 
 function Counter({ number, diff, onIncrease, onDecrease, onSetDiff }) {
@@ -22,10 +22,23 @@ function Counter({ number, diff, onIncrease, onDecrease, onSetDiff }) {
 function CounterContainer() {
   // useSelector: 리덕스 스토어 상태 조회 Hook
   // param으로 store.getState()로 호출되었을 때 값
-  const { number, diff } = useSelector(({ counter }) => ({
-    number: counter.number,
-    diff: counter.diff
-  }));
+  const { number, diff } = useSelector(
+    ({ counter }) => ({
+      number: counter.number,
+      diff: counter.diff
+    }),
+    shallowEqual
+  );
+
+  // useSelector Hook 을 통해 매번 렌더링 될 때마다 새로운 객체 { number, diff }를 생성
+  // 상태가 바뀌었는지 확인을 할 수 없어서 매번 새로 렌더링
+  // 아래 처럼 각각 따로 불러오던가
+  // const number = useSelector(state => state.counter.number);
+  // const diff = useSelector(state => state.counter.diff);
+  // 2번쨰 파라미터로 비교 하는 함수 추가
+  // (left, right) => { return left.number === right.number && left.diff === right.diff; }
+  // true가 나오면 리렌더링 X, false 서로 달라야지만 새로 렌더링
+  // react-redux에서 shallowEqual 함수 사용 가능
 
   // useDispatch: 리덕스 스토어의 dispatch 사용 할 수 있게 해주는 Hook
   const dispatch = useDispatch();
