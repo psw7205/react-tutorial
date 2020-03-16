0984a1779faa6503088d7218880fe9a88bde1222
psw7205-mac <psw7205@gmail.com>
2020-03-16 22:25:59 +0900

    Refactor redux-saga

--- react-middleware/src/lib/asyncUtils.js
+++ react-middleware/src/lib/asyncUtils.js
@@ -1,3 +1,32 @@
+import { call, put } from "redux-saga/effects";
+
+// redux-saga 사용 시
+export const createPromiseSaga = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return function*(action) {
+    try {
+      const payload = yield call(promiseCreator, action.payload);
+      yield put({ type: SUCCESS, payload });
+    } catch (e) {
+      yield put({ type: ERROR, error: true, payload: e });
+    }
+  };
+};
+
+export const createPromiseSagaById = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return function*(action) {
+    const id = action.meta;
+    try {
+      const payload = yield call(promiseCreator, action.payload);
+      yield put({ type: SUCCESS, payload, meta: id });
+    } catch (e) {
+      yield put({ type: ERROR, error: e, meta: id });
+    }
+  };
+};
+
+// redux-thunk 사용 시
 export const createPromiseThunk = (type, promiseCreator) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
 
